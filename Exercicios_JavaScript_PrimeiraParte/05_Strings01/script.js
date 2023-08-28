af44238dcf0a8839894d238c4a7d2812c8af8ad4
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-28 10:47:46 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_PrimeiraParte/05_Strings01/script.js
@@ -0,0 +1,7 @@
+console.log("Este é um texto");
+console.log('Este é um texto');
+console.log('Este é "um" texto');
+console.log("Este é 'um' texto");
+console.log(`Este é um texto`);
+console.log("Infinity");
+console.log(Infinity)
