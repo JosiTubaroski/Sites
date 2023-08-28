74c0dcd89447525542e3ac57ed6893daa5117f7a
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-28 10:43:46 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_PrimeiraParte/02_Number/script.js
@@ -0,0 +1,15 @@
+// Definindo numero inteiro - Para mostrar o tipo de dado no console utilizar o typeof
+
+console.log(typeof 12);
+
+// Definindo numero negativo
+
+console.log(typeof -12);
+
+// Definindo numero decimal
+
+console.log(typeof 1.17);
+
+// Definindo String
+
+console.log(typeof '1.17');
