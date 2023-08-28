21c6c36336afa74decbaf955dbf85bb32c380c74
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-28 10:51:51 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_PrimeiraParte/08_CompBoolean/script.js
@@ -0,0 +1,19 @@
+
+// Comparações
+
+// >        - MAIOR QUE
+// <        - MENOR QUE
+// >=       - MAIOR OU IGUAL
+// <=       - MENOR OU IGUAL
+// ==       - IGUAL
+// !=       - DIFERENTE
+// ===      - IDENTICO
+
+console.log(10 > 5); // TRUE
+console.log(50 < 100); // TRUE
+console.log(100 >= 100); // TRUE
+console.log(100 <= 50); // FALSE
+console.log(50 == 50); // TRUE
+console.log('vinicius' != 'vinicius'); // false
+console.log('5' === 5); // false
+console.log(5 === 5); // true
