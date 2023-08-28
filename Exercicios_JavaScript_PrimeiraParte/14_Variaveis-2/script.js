1dc35e3420fcbbfb6fb18a6001f39659ff815ffd
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-28 10:59:52 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_PrimeiraParte/14_Variaveis-2/script.js
@@ -0,0 +1,14 @@
+
+// var
+// let
+// const
+
+var variavel1 = 10;
+let variavel2 = 20;
+const variavel3 = 30; // Não é possivel reatribuir uma con
+
+console.log(variavel1);
+
+// let - modo mais recente de declarar variáveis
+
+
