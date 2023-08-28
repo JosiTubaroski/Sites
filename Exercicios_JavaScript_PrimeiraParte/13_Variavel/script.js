27b21a46f40540b30b0bc32bd4ab306fd728264f
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-28 10:58:06 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_PrimeiraParte/13_Variavel/script.js
@@ -0,0 +1,19 @@
+// Declarando Variaveis
+
+let preco = 5;
+console.log(preco);
+
+console.log(preco*preco);
+
+console.log(`O valor da sua compra foi de ${preco} reais`);
+
+preco = 10;
+
+console.log(`O valor da sua compra foi de ${preco} reais`);
+
+let num1 = 1, num2 = 2, num3 = 3;
+console.log(num1 + num2 + num3);
+
+let numero1 = 1;
+let numero2 = 2;
+let numero3 = 3;
