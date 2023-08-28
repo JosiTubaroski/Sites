3bbb22dfe63d2ef93cb5c15a11a34b0502549f35
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-28 11:02:32 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_PrimeiraParte/16_palavrasReservadas/script.js
@@ -0,0 +1,6 @@
+
+// Palavras reservadas
+
+// break case catch class const continue debugger default delete do else enum export extends false finally
+// for function if implements import interface in instanced let new package private protected public return
+// statis super switch this throw true try typeof var void while yield
