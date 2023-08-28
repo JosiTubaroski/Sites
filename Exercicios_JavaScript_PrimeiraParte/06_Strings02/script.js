ec8ebcc3cb0939459a701abfc53208fbe8933d4c
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-28 10:48:46 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_PrimeiraParte/06_Strings02/script.js
@@ -0,0 +1,10 @@
+// Colocar quebra de linha
+
+console.log("Este\n texto \n quebra\n a linha");
+
+var soma = 10 + 10;
+console.log(`A soma de 10 + 10 é ${soma}`)
+
+console.log('String'+'Concatenada')
+
+console.log('String '+'Concatenada '+'com '+ 'espaço')
