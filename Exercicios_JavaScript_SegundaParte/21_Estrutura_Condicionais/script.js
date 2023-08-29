4f1f94943eea5d1aa538eeceab6b383c6c4adeed
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-29 15:50:23 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/21_Estrutura_Condicionais/script.js
@@ -0,0 +1,19 @@
+
+let ticket = false;
+let idade = 20;
+
+if(ticket == true) {
+  console.log('Seja Bem vindo');
+}
+
+if(ticket == false){
+  console.log('Volte para sua casa');
+}
+
+if(idade >= 18){
+  console.log('Prossiga com a festa');
+} 
+
+if(idade < 18){
+  console.log('Você não tem a idade');
+}
