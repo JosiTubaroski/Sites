d8323458e7a40316ef7b7320aaf23df1d352c085
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-29 15:51:02 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/23_Estrutura_CondicionalElseIF/script.js
@@ -0,0 +1,15 @@
+
+let ticket = true;
+let idade = 20;
+let filhoDoDono = false;
+
+if(ticket == true && idade >= 18) {
+  console.log('Seja Bem vindo e prossiga na festa.');
+} else if (filhoDoDono) {
+  console.log('Prossiga e mande um abraço para o chefe.');
+}
+else {
+  console.log('Volte para sua casa');
+}
+
+
