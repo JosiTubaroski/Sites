ff2b9f28bf29c95bbbe25dbe711f015f27164afe
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-29 15:50:05 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/20_Built-in_math/script.js
@@ -0,0 +1,17 @@
+
+let number = Math.max(1, 15, 50, 7);
+
+console.log(number);
+
+let number2 = Math.min(1, 15, 50, 7)
+
+console.log(number2);
+
+let arredondr = Math.round(2.57);
+
+console.log(arredondr);
+
+let arredondrPracima = Math.ceil(2.27);
+
+
+console.log(arredondrPracima);
