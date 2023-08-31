b1134995d56ae70cabc4f49e3bb64a63fd56f10e
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-31 16:41:45 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/40_Array/script.js
@@ -0,0 +1,11 @@
+const arrayNumbers = [3, 6, 5, 4, 9]
+
+console.log(arrayNumbers);
+
+//-----------------------------------------------------------------------------------//
+
+const arrayNomes = ['Josi', 'Leandro', 'Debi', 'Tinho']
+
+console.log(arrayNomes[1])
+console.log(arrayNomes[arrayNomes.length - 1])
+console.log(arrayNomes.length - 1)
