09e00b43e7ebcc7000d22fdec59409aaee6dbe4c
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-31 16:36:38 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/27_laco_for/script.js
@@ -0,0 +1,13 @@
+
+for(let i = 0; i < 10; i = i + 1){
+  console.log(i)
+}
+
+console.log('------------------')
+
+let nomes = ['Vinicius', 'João', 'Andressa', 'Ricardo']
+
+for(let i = 0; i < nomes.length; i = i + 1){
+  console.log(nomes[i]);
+  
+}
