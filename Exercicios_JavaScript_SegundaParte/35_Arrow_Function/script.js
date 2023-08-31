88737d49f381d9d496ed67020ea19b8d7b5ddc56
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-31 16:40:03 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/35_Arrow_Function/script.js
@@ -0,0 +1,14 @@
+
+const animal = (name) => {
+  console.log(name);
+}
+
+animal('Cachorro');
+
+//----------------------------------------------------------------------------------------------//
+
+const soma = (number) => {
+  console.log(number + 5);
+}
+
+soma(10);
