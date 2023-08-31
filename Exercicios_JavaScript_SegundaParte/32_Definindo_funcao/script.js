8ddb3fb2497f06e98bdab68cda3d55077de23d20
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-31 16:39:06 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/32_Definindo_funcao/script.js
@@ -0,0 +1,26 @@
+
+// Criando uma função
+
+function imprimirMsgConsole() {
+  console.log('Esta é uma função')
+}
+
+// Invocando a função
+
+imprimirMsgConsole();
+
+function soma(a, b){
+  return a + b;
+}
+
+console.log(soma(1, 5));
+
+//--------------------------------------------------------------------------------------------//
+
+const cadastraUsuario = function(name) {
+  console.log(name);
+}
+
+cadastraUsuario('Josi');
+
+
