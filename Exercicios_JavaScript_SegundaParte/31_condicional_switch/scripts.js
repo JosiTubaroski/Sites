adcc1728873229b1e094b9ba91b6567dd5e0b129
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-31 16:38:44 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/31_condicional_switch/scripts.js
@@ -0,0 +1,15 @@
+let raca = 'Hotweil';
+
+switch(raca){
+  case 'Bigol':
+    console.log('A raça é Bigol');
+    break;
+  case 'Hotweiller':
+    console.log('A raça é Hotweiller');
+    break;  
+  case 'Pitbull':
+    console.log('A raça é Pitibul');
+    break;   
+  default:
+    console.log('Nenhuma raça encontrada');   
+}
