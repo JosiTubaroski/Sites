0daecf930eb33f9b50b3b233c593c7d7f3e1363b
Jozeliane (Josi) Quoos Tubaroski ✽ <66569714+JosiTubaroski@users.noreply.github.com>
2023-08-31 16:36:15 -0300

    Add files via upload

---
+++ Exercicios_JavaScript_SegundaParte/26_laco_do_while/script.js
@@ -0,0 +1,16 @@
+
+let count = 10;
+
+do {
+  count = count - 1;
+  console.log(count);
+} while (count > 0);
+
+console.log('---------');
+
+let x = 0;
+
+do {
+  x = x + 1;
+  console.log(x);
+} while (x < 50);
